babf1d68d64fa0346da25ceec6cae5ffb548e9ee
Tinnevdabb <tinne.vdabb@skynet.be>
2016-11-09 19:12:10 +0200

    Added route to latestupdates (main page)

--- imports/lib/routes.js
+++ imports/lib/routes.js
@@ -2,3 +2,11 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../ui/latestupdates.js';
+
+
+FlowRouter.route('/',{
+	name: 'latestupdates',
+  	action() {
+    	BlazeLayout.render('latestupdates');
+  	},
+});
