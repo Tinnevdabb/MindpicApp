7fd488d811abd9c05468b062248c3636888e578e
kehv <kiprotichkevin56@gmail.com>
2016-11-17 09:34:10 +0200

    Added main route (login)

--- imports/lib/routes.js
+++ imports/lib/routes.js
@@ -1,12 +1,20 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-import '../ui/latestupdates.js';
 
+import '../ui/latestupdates.js';
+import '../ui/login.js';
 
 FlowRouter.route('/',{
+	name: 'login',
+  	action() {
+    	BlazeLayout.render('login');
+  	},
+});
+
+FlowRouter.route('/latestupdates',{
 	name: 'latestupdates',
   	action() {
-    	BlazeLayout.render('latestupdates');
+    	BlazeLayout.render('latestupdates',{main:'login'});
   	},
 });
