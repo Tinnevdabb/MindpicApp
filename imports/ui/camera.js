8d7023eb0a61f4d10995844ecf8a2598ed678873
kehv <kiprotichkevin56@gmail.com>
2016-11-21 14:38:23 +0200

    Calls camera(isn't working)

--- imports/ui/camera.js
+++ imports/ui/camera.js
@@ -4,14 +4,23 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import './camera.html';
 
 
-Template.example.events({
-    'click .takePhoto': function(event, template) {
-        var cameraOptions = {
-            width: 800,
-            height: 600
-        };
-        MeteorCamera.getPicture(cameraOptions, function (error, data) {
+if (Meteor.isClient) {
+  Template.camera.helpers({
+    photo: function () {
+      return Session.get("photo");
+    }
+  });
+
+  Template.camera.events({
+    'click button': function () {
+      var cameraOptions = {
+        width: 800,
+        height: 600
+      };
+
+      MeteorCamera.getPicture(cameraOptions, function (error, data) {
         Session.set("photo", data);
       });
     }
   });
+}
