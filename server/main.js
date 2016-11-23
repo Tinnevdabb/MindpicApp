f6b7125f9a6e5f34f8345a79caaa91cb335bb180
kehv <kiprotichkevin56@gmail.com>
2016-11-23 10:49:20 +0200

    added database code for saving in the camera

--- server/main.js
+++ server/main.js
@@ -3,3 +3,12 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Pictures = new Mongo.Collection('pictures');
+
+Meteor.methods({
+    saveImage(data) {
+        console.log("[Server] - Save picture???");
+        Pictures.insert({image: data});
+    }
+});
