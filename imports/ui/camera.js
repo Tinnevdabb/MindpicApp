9ebfdef243c5e4a587557ab069558ca0bab70f3f
kehv <kiprotichkevin56@gmail.com>
2016-11-23 10:43:27 +0200

    added htmls and js file for camera

--- imports/ui/camera.js
+++ imports/ui/camera.js
@@ -1,26 +1,28 @@
+
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import './camera.html';
 
 
-if (Meteor.isClient) {
-  Template.camera.helpers({
-    photo: function () {
-      return Session.get("photo");
-    }
-  });
+Pictures = new Mongo.Collection('pictures');
 
-  Template.camera.events({
-    'click button': function () {
-      var cameraOptions = {
-        width: 800,
-        height: 600
-      };
-
-      MeteorCamera.getPicture(cameraOptions, function (error, data) {
-        Session.set("photo", data);
-      });
+Template.takePhoto.helpers({
+    'photos': function () {
+        return Pictures.find();
     }
-  });
+});
+
+Template.takePhoto.events({
+    'click .capture': function () {
+        var cameraoptions = {
+            width: 640,
+            height: 480
+        };
+
+        MeteorCamera.getPicture(cameraoptions, function (error, data) {
+        //Session.set('photo', data);
+        Meteor.call('saveImage', (data));
+    });
 }
+});
