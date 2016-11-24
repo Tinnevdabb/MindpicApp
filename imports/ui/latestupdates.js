8bfabf4ada0034fbcd9eebc487f52f72aa82730f
Tinnevdabb <tinne.vdabb@skynet.be>
2016-11-24 14:52:08 +0200

    made images responsive

--- imports/ui/latestupdates.js
+++ imports/ui/latestupdates.js
@@ -2,3 +2,10 @@ import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import './latestupdates.html';
+
+
+$(document).ready(function () {
+    $('.glyphicon').click(function () {
+        $(this).toggleClass("glyphicon-star-empty").toggleClass("glyphicon-star");
+    });
+});
