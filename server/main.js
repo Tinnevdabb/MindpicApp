4d0b21592b95c5e4a9093e663c2eff2f92f7cc97
Tinnevdabb <tinne.vdabb@skynet.be>
2016-11-29 13:44:27 +0200

    Added import to image collection js file

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
+import '../imports/lib/image.collection.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
@@ -8,7 +10,7 @@ pic = new Mongo.Collection('pictures');
 
 Meteor.methods({
     saveImage(data) {
-        console.log("[Server] - Save picture???"); //see if it goes inside 
+        console.log("[Server] - Save picture???"); //see if it goes inside
         pic.insert({image: data});
     }
 });
