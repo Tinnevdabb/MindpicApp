a2b7b09b3f8b6f3b0fdd8135b26dc55172edec31
Tinnevdabb <tinne.vdabb@skynet.be>
2016-11-29 11:35:50 +0200

    Upload image html and js update. We can upload image and show link and download link

--- imports/ui/uploadimage.js
+++ imports/ui/uploadimage.js
@@ -1,5 +1,55 @@
 import { Template } from 'meteor/templating';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './uploadimage.html';
+
+import {Images} from '../lib/image.collection.js';
+
+
+Template.uploadedFiles.helpers({
+  uploadedFiles: function () {
+    return Images.find();
+  }
+});
+
+Template.uploadForm.onCreated(function () {
+  this.currentUpload = new ReactiveVar(false);
+});
+
+Template.uploadForm.helpers({
+  currentUpload: function () {
+    return Template.instance().currentUpload.get();
+  }
+});
+
+Template.uploadForm.events({
+  'change #fileInput': function (e, template) {
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      // We upload only one file, in case
+      // there was multiple files selected
+      var file = e.currentTarget.files[0];
+      if (file) {
+        var uploadInstance = Images.insert({
+          file: file,
+          streams: 'dynamic',
+          chunkSize: 'dynamic'
+        }, false);
+
+        uploadInstance.on('start', function() {
+          template.currentUpload.set(this);
+        });
+
+        uploadInstance.on('end', function(error, fileObj) {
+          if (error) {
+            alert('Error during upload: ' + error.reason);
+          } else {
+            alert('File "' + fileObj.name + '" successfully uploaded');
+          }
+          template.currentUpload.set(false);
+        });
+
+        uploadInstance.start();
+      }
+    }
+  }
+});
